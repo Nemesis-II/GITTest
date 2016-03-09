051d9770a1904d392a67f448a986afbfd7776bf5
An Truong <Nemesis@ans-mac-pro.local>
2016-03-09 09:19:04 +0700

    Add Test Class

---
+++ GITTest/TestClass.h
@@ -0,0 +1,13 @@
+//
+//  TestClass.h
+//  GITTest
+//
+//  Created by An Truong on 3/9/16.
+//  Copyright © 2016 An Truong. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TestClass : NSObject
+
+@end
---
+++ GITTest/TestClass.m
@@ -0,0 +1,13 @@
+//
+//  TestClass.m
+//  GITTest
+//
+//  Created by An Truong on 3/9/16.
+//  Copyright © 2016 An Truong. All rights reserved.
+//
+
+#import "TestClass.h"
+
+@implementation TestClass
+
+@end
